f6f1fb5d93a1bdd251832b9be27323d6f6aafc6c
Jefferson Valencia Gómez <jevago262000@gmail.com>
2024-11-29 20:21:01 -0500

    .

---
+++ otherfunctions.py
@@ -0,0 +1,155 @@
+def water_balance(image, image_list, ee, whc, k):
+    
+      # Determine the date of the current ee.Image of the collection.
+    localdate = image.get('system:time_start')
+
+    # Import previous image stored in the list
+    prev_im = ee.Image(ee.List(image_list).get(-1))
+
+    # Import previous soil storage and baseflow
+    # Only those variables of the water balance that work with previous conditions (i.e., i-1)
+    prev_st = prev_im.select("sstor")
+    prev_bf = prev_im.select("bflow")
+
+    # Import current precipitation, potential evapotranspiration, and runoff
+    pr_im = image.select("pr")
+    pet_im = image.select("pet").multiply(0.1) # Scale factor: 0.1
+    ro_im = image.select('ro')
+
+    # Initialize the new bands associated with wyield, eprec, aet, perc, sstor, and bflow
+    new_wy = (
+        ee.Image(0)
+        .set("system:time_start", localdate)
+        .select([0], ["wyield"])
+        .float()
+    )
+
+    new_ep = (
+        pr_im.subtract(ro_im)
+        .set("system:time_start", localdate)
+        .select([0], ["eprec"])
+        .float()
+    )
+
+    new_aet = (
+        ee.Image(0)
+        .set("system:time_start", localdate)
+        .select([0], ["aet"])
+        .float()
+    )
+
+    new_pc = (
+        ee.Image(0)
+        .set("system:time_start", localdate)
+        .select([0], ["perc"])
+        .float()
+    )
+
+    new_st = (
+        ee.Image(0)
+        .set("system:time_start", localdate)
+        .select([0], ["sstor"])
+        .float()
+    )
+
+    new_bf = (
+        ee.Image(0)
+        .set("system:time_start", localdate)
+        .select([0], ["bflow"])
+        .float()
+    )
+
+    # Calculate bands depending on the situation using binary layers with logical operations
+
+    ## CASE 1
+    # Define zone1: pixels where EPREC > PET 
+    zone1 = new_ep.gt(pet_im)
+
+    # Calculation of AET in zone 1
+    zone1_aet = pet_im.rename("aet")
+    # Implementation of zone 1 values for AET
+    new_aet = new_aet.where(zone1, zone1_aet)
+
+    ## CASE 1.1
+    # Define zone11: pixels where (EPREC > PET) and (Si-1 + EPREC - AET > WHC)
+    zone11 = zone1.And(prev_st.add(new_ep).subtract(new_aet).gt(whc))
+
+    # Calculation of SSTOR in zone 1.1
+    zone11_st = whc.rename("sstor")
+    # Implementation of zone 1.1 values for SSTOR
+    new_st = new_st.where(zone11, zone11_st)
+
+    # Calculation of PERC in zone 1.1
+    zone11_pc = prev_st.add(new_ep).subtract(new_aet).subtract(whc).rename("perc")
+    # Implementation of zone 1.1 values for PERC
+    new_pc = new_pc.where(zone11, zone11_pc)
+
+    ## CASE 1.2
+    # Define zone12: pixels where (EPREC > PET) and (Si-1 + EPREC - AET <= WHC)
+    zone12 = zone1.And(prev_st.add(new_ep).subtract(new_aet).lte(whc))
+
+    # Calculation of SSTOR in zone 1.2
+    zone12_st = prev_st.add(new_ep).subtract(new_aet).rename("sstor")
+    # Implementation of zone 1.2 values for SSTOR
+    new_st = new_st.where(zone12, zone12_st)
+
+    ## CASE 2
+    # Define zone2: pixels where EPREC <= PET
+    zone2 = new_ep.lte(pet_im)
+
+    # Calculation of SSTOR in zone 2
+    zone2_st = prev_st.multiply(ee.Image.exp(new_ep.subtract(pet_im).abs().divide(whc).multiply(-1))).rename("sstor")
+    # Implementation of zone 2 values for SSTOR
+    new_st = new_st.where(zone2, zone2_st)
+
+    # Calculation of AET in zone 2
+    zone2_aet = new_ep.add(new_st).subtract(prev_st).rename("aet")
+    # Implementation of zone 2 values for AET
+    new_aet = new_aet.where(zone2, zone2_aet)
+
+    new_bf_p1 = prev_bf.multiply(k)
+    new_bf_p2 = ee.Image(1).subtract(k).multiply(new_pc)
+    new_bf = new_bf.add(new_bf_p1).add(new_bf_p2).rename("bflow")
+
+    new_wy = new_wy.add(new_bf).add(ro_im).rename("wyield")
+
+    # Create a mask around area where pixels can effectively be calculated
+    # Where we have have PREC as TerraClimate and WHC
+    mask = pr_im.gte(0).And(whc.gte(0))
+    
+    # Apply the mask
+    new_wy = new_wy.updateMask(mask)
+
+    # Add all Bands to our ee.Image
+    new_image = new_wy.addBands(ee.Image([pr_im, pet_im, ro_im, new_ep, new_aet, new_st, new_pc, new_bf]))
+    new_image = new_image.set("system:time_start", localdate) # Needed to have 'start_time' finally assigned correctly
+    
+    # Add the new ee.Image to the ee.List
+    return ee.List(image_list).add(new_image)
+
+
+def ee_array_to_df(arr, list_of_bands):
+
+    import pandas as pd
+
+    """Transforms client-side ee.Image.getRegion array to pandas.DataFrame."""
+    df = pd.DataFrame(arr)
+
+    # Rearrange the header.
+    headers = df.iloc[0]
+    df = pd.DataFrame(df.values[1:], columns=headers)
+
+    # Convert the data to numeric values.
+    for band in list_of_bands:
+        df[band] = pd.to_numeric(df[band], errors="coerce")
+
+    # Convert the time field into a datetime.
+    df["datetime"] = pd.to_datetime(df["time"], unit="ms").dt.strftime("%Y-%m")
+
+    # Keep the columns of interest.
+    df = df[["time", "datetime", *list_of_bands]]
+
+    # The datetime column is defined as index.
+    df = df.set_index("datetime")
+
+    return df
